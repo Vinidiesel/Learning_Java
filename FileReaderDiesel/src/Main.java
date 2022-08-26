d1faae6f0699e0265e31c1ab594c1b11383dab99
vinicius <inhesta10@gmail.com>
2022-08-26 11:49:08 -0300

    Buff

--- FileReaderDiesel/src/Main.java
+++ FileReaderDiesel/src/Main.java
@@ -4,12 +4,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "C:\\temp\\in.txt";
-        BufferedReader br = null;
-        FileReader fr = null;
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        String path = "C:\\Users\\vinic\\Learning_Java\\in.txt";
+
+        try (BufferedReader br = new BufferedReader( new FileReader(path))) {
             String line = br.readLine();
             while (line != null) {
                 System.out.println(line);
@@ -17,15 +14,6 @@ public class Main {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null)
-                    br.close();
-                if (fr != null)
-                    fr.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
