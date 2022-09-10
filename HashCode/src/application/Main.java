d32f66837a3a2998447c4a0889e60c3f415d40e7
vinicius <inhesta10@gmail.com>
2022-09-10 02:07:07 -0300

    ola

---
+++ HashCode/src/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.Client;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Client c1 = new Client("Maria","maria@gmail.com");
+        Client c2 = new Client("Maria","maria@gmail.com");
+
+        String s1 = "Test";
+        String s2 = "Test";
+        String s3 = new String("Test");
+        String s4 = new String("Test");
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
+        System.out.println(s1 == s2);
+        System.out.println(s3 == s4);
+    }
+}
