2660d418c2d9e2ee6918070eaf1a676450876ea0
vinicius <inhesta10@gmail.com>
2022-09-10 12:54:29 -0300

    ola

---
+++ LogRegisters/src/application/Main.java
@@ -0,0 +1,50 @@
+package application;
+
+import entities.LogEntry;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter file full path: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
+            Set<LogEntry> set = new HashSet<>();
+
+            String line = br.readLine();
+            while (line != null){
+
+                String[] fields = line.split(" ");
+                String username = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+
+                set.add(new LogEntry(username,moment));
+
+                line = br.readLine();
+
+            }
+
+            System.out.println("Total users: " + set.size());
+
+        }catch (IOException e){
+
+            System.out.println("Error: " + e.getMessage());
+
+        }
+
+        sc.close();
+    }
+}
+//C:\Users\vinic\Learning_Java\LogRegisters\in.txt
